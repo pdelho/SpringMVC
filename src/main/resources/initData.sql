20d5652abc70f3f29863dda3247e4f72f0d123a4
pablo <pablodelhoyo@gmail.com>
2017-08-12 18:06:48 +0200

    Database configuration and initializacion

--- src/main/resources/initData.sql
+++ src/main/resources/initData.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO persons (id, firstname, lastname, description, age) values(0, 'Eduardo', 'I', 'El gato', 2);
+INSERT INTO persons (id, firstname, lastname, description, age) values(0, 'Eduardo', 'I', 'El gato', 2);
 INSERT INTO persons (id, firstname, lastname, description, age) values(1, 'Eduardo', 'II', 'El novio', 25);
 INSERT INTO persons (id, firstname, lastname, description, age) values(2, 'Pablo', 'Molinos', 'El abuelo', 25);
 INSERT INTO persons (id, firstname, lastname, description, age) values(3, 'Pablo', 'Molinos', 'El abuelo', 25);
